b962971dd0e01d7fc2402c7eed83b09d78a50968
liho <liho@revenuemonster.my>
2025-01-11 23:50:04 +0800

    Refactor next.config.mjs to clean up formatting

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // basePath: "/eva-ui",
-    output: "export",
-    // reactStrictMode: true,
-    images: {
-        unoptimized: true,
-      },
+  // basePath: "/eva-ui",
+  output: "export",
+  // reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
