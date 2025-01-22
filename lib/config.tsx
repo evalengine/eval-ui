6f7f2a2efba19f869e38885493e69fc7af152e33
Johnson Lai <jlwhoo7@gmail.com>
2025-01-22 09:34:04 -0500

    update links

--- lib/config.tsx
+++ lib/config.tsx
@@ -124,17 +124,18 @@ export const siteConfig = {
     socialLinks: [
       {
         icon: <Icons.github className="h-5 w-5" />,
-        url: "https://github.com/chromindscan",
+        url: "https://github.com/evalengine",
       },
       {
         icon: <Icons.twitter className="h-5 w-5" />,
-        url: "https://x.com/jlwhoo7",
+        url: "https://x.com/Eval_Engine ",
       },
     ],
     links: [
       // { text: "Privacy Policy", url: "/legal/privacy" },
       // { text: "Terms of Service", url: "/legal/terms" },
-      { text: "Contact", url: "#" },
+      // { text: "Contact", url: "#" },
+      { text: "Docs", url: "https://eval-engine.gitbook.io/eval-engine" },
     ],
     bottomText: "© 2025 EVALEngine. All rights reserved.",
     brandText: "EVAL Engine",
