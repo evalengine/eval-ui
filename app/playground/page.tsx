418b4251e0f1a24a5a52e7ba8080760516da842b
liho <liho@revenuemonster.my>
2025-01-22 20:48:29 +0800

    feat: add Card3 component and update layout in Tools section

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -2,14 +2,15 @@
 
 import { cubicBezier, motion } from "framer-motion";
 import Link from "next/link";
-import { Card1, Card2 } from "@/components/sections/tools";
+import { Card1, Card2, Card3 } from "@/components/sections/tools";
 
 export default function Home() {
   return (
-    <div className="h-[calc(100dvh-56px)] w-full max-w-screen-lg mx-auto flex items-center justify-center">
-      <div className="grid lg:grid-cols-2 rounded-xl border w-full">
+    <div className="lg:h-[calc(100dvh-56px)] w-full max-w-screen-lg mx-auto flex items-center justify-center">
+      <div className="grid lg:grid-cols-3 rounded-xl border w-full">
         <Card1 />
         <Card2 />
+        <Card3 />
       </div>
     </div>
   );
--- components/sections/tools.tsx
+++ components/sections/tools.tsx
@@ -241,7 +241,7 @@ export const Card2 = () => {
   };
   return (
     <Link href="/playground/virtuals">
-      <div className="p-0 h-full overflow-hidden border-b lg:border-b-0">
+      <div className="p-0 h-full overflow-hidden border-b lg:border-b-0 lg:border-r">
         <motion.div
           variants={containerVariants}
           initial="initial"
@@ -291,6 +291,115 @@ export const Card2 = () => {
           <div className="flex min-h-[100px] w-full flex-col items-start border-t border-neutral-200 p-4 dark:border-neutral-800">
             <h2 className="text-xl font-semibold"> Virtuals Sandbox</h2>
             <p className="text-base font-normal text-neutral-500 dark:text-neutral-400">
+              Interact with Virtual characters, and evaluate.
+            </p>
+          </div>
+        </motion.div>
+      </div>
+    </Link>
+  );
+};
+
+export const Card3 = () => {
+  const variant1 = {
+    initial: {
+      viewBox: "0 -950 366 1408",
+      filter: "saturate(0.3)",
+      opacity: 0.5,
+      transition: {
+        delay: 0.05,
+        duration: 0.3,
+        ease: cubicBezier(0.22, 1, 0.36, 1),
+      },
+    },
+    whileHover: {
+      viewBox: "0 -60 366 310",
+      filter: "saturate(1)",
+      opacity: 1,
+      transition: {
+        delay: 0.05,
+        duration: 0.3,
+        ease: cubicBezier(0.22, 1, 0.36, 1),
+      },
+    },
+  };
+  const variant2 = {
+    initial: {
+      y: 0,
+      transition: {
+        delay: 0.05,
+        duration: 0.3,
+        ease: cubicBezier(0.22, 1, 0.36, 1),
+      },
+    },
+    whileHover: {
+      y: 0,
+      transition: {
+        delay: 0.05,
+        duration: 0.3,
+        ease: cubicBezier(0.22, 1, 0.36, 1),
+      },
+    },
+  };
+
+  const containerVariants = {
+    initial: {},
+    whileHover: {
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+  return (
+    <Link href="/analytics">
+      <div className="p-0 h-full overflow-hidden border-b lg:border-b-0">
+        <motion.div
+          variants={containerVariants}
+          initial="initial"
+          whileHover="whileHover"
+          className="flex h-full w-full cursor-pointer flex-col items-start justify-between"
+        >
+          <div className="flex h-full w-full items-center justify-center rounded-t-xl bg-transparent">
+            <div className="relative flex h-full w-full cursor-pointer items-center justify-center overflow-hidden rounded-t-xl bg-transparent p-10">
+              <div className="relative h-[150px] w-full cursor-pointer overflow-hidden rounded-xl border border-slate-200/50 bg-white dark:border-neutral-700/50 dark:bg-neutral-900">
+                <motion.p
+                  variants={variant2}
+                  className="absolute left-5 top-5 w-fit text-[15px]"
+                >
+                  +245%
+                </motion.p>
+                <motion.svg
+                  variants={variant1}
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  preserveAspectRatio="none"
+                  className="absolute inset-0 h-full w-full"
+                  viewBox="0 -950 366 1408"
+                >
+                  <path
+                    fill="url(#a)"
+                    d="M0 193c109.5 0 260.5-52.5 366-192.5v907H0V193Z"
+                  />
+                  <defs>
+                    <linearGradient
+                      id="a"
+                      x1={183}
+                      x2={183}
+                      y1={0.5}
+                      y2={262}
+                      gradientUnits="userSpaceOnUse"
+                    >
+                      <stop stopColor="#22c55e" />
+                      <stop offset={1} stopColor="#15803d" />
+                    </linearGradient>
+                  </defs>
+                </motion.svg>
+              </div>
+            </div>
+          </div>
+          <div className="flex min-h-[100px] w-full flex-col items-start border-t border-neutral-200 p-4 dark:border-neutral-800">
+            <h2 className="text-xl font-semibold">Analytics</h2>
+            <p className="text-base font-normal text-neutral-500 dark:text-neutral-400">
               Track progress and performance.
             </p>
           </div>
@@ -303,9 +412,10 @@ export const Card2 = () => {
 export function Tools() {
   return (
     <Section id="use-cases">
-      <div className="grid lg:grid-cols-2 h-full border border-b-0">
+      <div className="grid lg:grid-cols-3 h-full border border-b-0">
         <Card1 />
         <Card2 />
+        <Card3 />
       </div>
     </Section>
   );
