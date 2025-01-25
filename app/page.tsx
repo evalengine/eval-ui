244763fc2f523dc17139672f30f66a72f31a412e
liho <liho@revenuemonster.my>
2025-01-26 00:06:05 +0800

    refactor: update Home component to use useSearchParams and useEffect for token handling feat: set output to "export" in next.config.mjs

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { CTA } from "@/components/sections/cta";
 import { Features } from "@/components/sections/features";
 import { Hero } from "@/components/sections/hero";
@@ -5,17 +6,27 @@ import { Logos } from "@/components/sections/logos";
 import { Tools } from "@/components/sections/tools";
 import { UseCases } from "@/components/sections/use-cases";
 import { Chromia } from "@/components/sections/chromia";
-import { redirect } from "next/navigation";
+import { redirect, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: Promise<{ token: string }>;
-}) {
-  const { token } = await searchParams;
-  if (token) {
-    redirect(`/playground/virtuals?token=${token}`);
-  }
+export default function Home() {
+  // {
+  //   searchParams,
+  // }: {
+  //   searchParams: Promise<{ token: string }>;
+  // }
+  const searchParams = useSearchParams();
+  useEffect(() => {
+    const token = searchParams.get("token");
+    if (token) {
+      redirect(`/playground/virtuals?token=${token}`);
+    }
+  }, [searchParams]);
+
+  // const { token } = await searchParams;
+  // if (token) {
+  //   redirect(`/playground/virtuals?token=${token}`);
+  // }
 
   return (
     <>
