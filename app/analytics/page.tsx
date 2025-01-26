49466c53782ed10fc94cad97ce27a3b7ec1dd45a
liho <liho@revenuemonster.my>
2025-01-26 22:50:09 +0800

    refactor: simplify DashboardPage component by removing unused imports and commented code

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -1,47 +1,21 @@
 import { Metadata } from "next";
-import Image from "next/image";
 
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-// import { CalendarDateRangePicker } from "./components/date-range-picker"
-import { RecentLogs } from "./components/recent-logs";
-import { Skeleton } from "@/components/ui/skeleton";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { ArrowUpRightFromSquare } from "lucide-react";
 import Link from "next/link";
+import { Metrics } from "./components/metrics";
+import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: "Dashboard",
   description: "Example dashboard app built using the components.",
 };
-import { Metrics } from "./components/metrics";
-import { Analytics } from "./components/analytics";
 
-export default function DashboardPage({
-  searchParams,
-}: {
-  searchParams: URLSearchParams;
-}) {
+export default function DashboardPage() {
   return (
     <>
       <div className="container mx-auto flex-col md:flex">
-        {/* <div className="border-b">
-          <div className="flex h-16 items-center px-4">
-            <TeamSwitcher />
-            <MainNav className="mx-6" />
-            <div className="ml-auto flex items-center space-x-4">
-              <Search />
-              <UserNav />
-            </div>
-          </div>
-        </div> */}
         <div className="flex-1 space-y-4 pt-6">
           <Link
             href={
@@ -66,25 +40,9 @@ export default function DashboardPage({
             <TabsList>
               <TabsTrigger value="overview">Overview</TabsTrigger>
               <TabsTrigger value="analytics">Analytics</TabsTrigger>
-              {/* <TabsTrigger value="reports" disabled>
-                Reports
-              </TabsTrigger> */}
             </TabsList>
             <TabsContent value="overview" className="space-y-4">
               <Metrics />
-              {/* <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
-                <Card className="col-span-full">
-                  <CardHeader>
-                    <CardTitle>Recent logs</CardTitle>
-                    <CardDescription>
-                      The most recent logs from your operations.
-                    </CardDescription>
-                  </CardHeader>
-                  <CardContent>
-                    <RecentLogs />
-                  </CardContent>
-                </Card>
-              </div> */}
             </TabsContent>
             <TabsContent value="analytics">
               <Analytics />
