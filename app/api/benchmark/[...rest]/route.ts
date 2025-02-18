e73b41d7193785a75dd97e78adcf79e71c9565cc
liho <liho@revenuemonster.my>
2025-02-19 01:36:48 +0800

    refactor: comment out unused POST route in benchmark API

--- app/api/benchmark/[...rest]/route.ts
+++ app/api/benchmark/[...rest]/route.ts
@@ -1,36 +1,48 @@
 import { NextResponse } from "next/server";
-import axios, { AxiosResponse } from "axios";
+// import axios, { AxiosResponse } from "axios";
 
 export async function GET(req) {
   try {
     const url = new URL(req.url);
     const pathname = url.pathname.replaceAll("/api/benchmark", "");
-    const response = await axios.get(pathname, {
-      baseURL: process.env.NEXT_PUBLIC_EVAL_ENGINE_BENCHMARK_API_URL,
+
+    // const response = await axios.get(pathname, {
+    //   baseURL: process.env.NEXT_PUBLIC_EVAL_ENGINE_BENCHMARK_API_URL,
+    //   headers: {
+    //     "X-API-Key": process.env.EVAL_ENGINE_ADMIN_API_KEY,
+    //   }
+    // });
+    const response = await fetch(`${process.env.NEXT_PUBLIC_EVAL_ENGINE_BENCHMARK_API_URL}${pathname}`, {
+      method: "GET",
       headers: {
-        "X-API-Key": process.env.EVAL_ENGINE_ADMIN_API_KEY,
+        "X-API-Key": process.env.EVAL_ENGINE_ADMIN_API_KEY!,
       }
     });
-    return NextResponse.json(response.data, { status: response.status });
+    if (!response.ok) {
+      throw response;
+    }
+    const data = await response.json();
+    return NextResponse.json(data, { status: response.status });
 
-  } catch (error: AxiosResponse | any) {
-    return NextResponse.json(error.response.data, { status: error.status });
+  } catch (error: Response | any) {
+    const data = await error.json();
+    return NextResponse.json(data, { status: error.status });
   }
 }
 
-export async function POST(req) {
-  try {
-    const url = new URL(req.url);
-    const pathname = url.pathname.replaceAll("/api/benchmark", "");
-    const response = await axios.post(pathname, req.body, {
-      baseURL: process.env.NEXT_PUBLIC_EVAL_ENGINE_BENCHMARK_API_URL,
-      headers: {
-        "X-API-Key": process.env.EVAL_ENGINE_ADMIN_API_KEY,
-      }
-    });
-    return NextResponse.json(response.data, { status: response.status });
+// export async function POST(req) {
+//   try {
+//     const url = new URL(req.url);
+//     const pathname = url.pathname.replaceAll("/api/benchmark", "");
+//     const response = await axios.post(pathname, req.body, {
+//       baseURL: process.env.NEXT_PUBLIC_EVAL_ENGINE_BENCHMARK_API_URL,
+//       headers: {
+//         "X-API-Key": process.env.EVAL_ENGINE_ADMIN_API_KEY,
+//       }
+//     });
+//     return NextResponse.json(response.data, { status: response.status });
 
-  } catch (error: AxiosResponse | any) {
-    return NextResponse.json(error.response.data, { status: error.status });
-  }
-}
+//   } catch (error: AxiosResponse | any) {
+//     return NextResponse.json(error.response.data, { status: error.status });
+//   }
+// }
