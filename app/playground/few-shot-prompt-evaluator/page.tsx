8c3379113164e05c04a26744ba06ec35f4fcf0c0
liho <liho@revenuemonster.my>
2025-02-26 02:02:14 +0800

    refactor: remove unused VercelIcon component and clean up imports in few-shot prompt evaluator

--- app/playground/few-shot-prompt-evaluator/page.tsx
+++ app/playground/few-shot-prompt-evaluator/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Suspense } from "react";
 import { PromptEditor } from "@/components/prompt-editor";
 
 export default function Page() {
--- components/prompt-editor.tsx
+++ components/prompt-editor.tsx
@@ -789,21 +789,6 @@ function ImprovedExamples({
   );
 }
 
-export function VercelIcon(props) {
-  return (
-    <svg
-      viewBox="0 0 256 222"
-      width="256"
-      height="222"
-      xmlns="http://www.w3.org/2000/svg"
-      preserveAspectRatio="xMidYMid"
-      {...props}
-    >
-      <path fill="#000" d="m128 0 128 221.705H0z" />
-    </svg>
-  );
-}
-
 // Add this new component for displaying code snippets
 function CodeSnippet({
   systemPrompt,
